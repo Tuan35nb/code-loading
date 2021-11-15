576847b94f425d46baaf9169b3a55da00c329fc8
Tuan35nb <94367137+Tuan35nb@users.noreply.github.com>
2021-11-15 19:39:51 +0700

    Add files via upload

---
+++ hack.bat
@@ -0,0 +1,5 @@
+:@echo off
+color 0c
+:a
+echo%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+goto a
